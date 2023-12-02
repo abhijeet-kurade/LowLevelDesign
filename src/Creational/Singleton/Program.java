b00e8d890311bad522a56cb07f7cf8f632738729
Abhijeet B Kurade <64726465+abhikurade@users.noreply.github.com>
2023-12-02 17:15:43 +0530

    Creational: Singleton design pattern.

---
+++ src/Creational/Singleton/Program.java
@@ -0,0 +1,19 @@
+package Creational.Singleton;
+
+public class Program {
+}
+
+class Singleton{
+    private static Singleton instance;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getInstance(){
+        if(instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
